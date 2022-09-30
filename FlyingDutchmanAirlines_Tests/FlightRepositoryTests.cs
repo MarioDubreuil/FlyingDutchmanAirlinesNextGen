d80304cbd770d61a2faf2bd458c68107d47df8f6
Mario Dubreuil <mario_dubreuil@hotmail.com>
2022-09-30 14:35:04 -0400

    Add tetst for get flight by flight number

--- FlyingDutchmanAirlines_Tests/FlightRepositoryTests.cs
+++ FlyingDutchmanAirlines_Tests/FlightRepositoryTests.cs
@@ -1,4 +1,6 @@
 ﻿using FlyingDutchmanAirlines.DatabaseLayer;
+using FlyingDutchmanAirlines.DatabaseLayer.Models;
+using FlyingDutchmanAirlines.Exceptions;
 using FlyingDutchmanAirlines.RepositoryLayer;
 using FlyingDutchmanAirlines_Tests.Stubs;
 using Microsoft.EntityFrameworkCore;
@@ -20,6 +22,15 @@ public class FlightRepositoryTests
                 .Options;
         _context = new FlyingDutchmanAirlinesContext_Stub(dbContextOptions);
 
+        var flight = new Flight
+        {
+            FlightNumber = 1,
+            Origin = 1,
+            Destination = 2
+        };
+        _context.Flights.Add(flight);
+        await _context.SaveChangesAsync();
+
         _repository = new FlightRepository(_context);
         Assert.IsNotNull(_repository);
     }
@@ -33,5 +44,27 @@ public class FlightRepositoryTests
     public async Task GetFlightByflightNumber_Failure_InvalidArguments(int flightNumber, int origin, int destination)
     {
         await _repository.GetFlightByFlightNumber(flightNumber, origin, destination);
+    }
+
+    [TestMethod]
+    [DataRow(0, 0, 0)]
+    [DataRow(1, 1, 1)]
+    [ExpectedException(typeof(FlightNotFoundException))]
+    public async Task GetFlightByflightNumber_Failure_FlightNotFound(int flightNumber, int origin, int destination)
+    {
+        await _repository.GetFlightByFlightNumber(flightNumber, origin, destination);
+    }
+
+    [TestMethod]
+    [DataRow(1, 1, 2)]
+    public async Task GetFlightByflightNumber_Success(int flightNumber, int origin, int destination)
+    {
+        var flight = await _repository.GetFlightByFlightNumber(flightNumber, origin, destination);
+        Assert.IsNotNull(flight);
+
+        var dbFlight = await _context.Flights.FirstAsync(f => f.FlightNumber == flightNumber);
+        Assert.AreEqual(dbFlight.FlightNumber, flight.FlightNumber);
+        Assert.AreEqual(dbFlight.Origin, flight.Origin);
+        Assert.AreEqual(dbFlight.Destination, flight.Destination);
     }
 }
