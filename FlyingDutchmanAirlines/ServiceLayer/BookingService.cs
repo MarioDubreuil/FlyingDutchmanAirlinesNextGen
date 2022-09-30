f3eadb86ecb79a5d72345b29a26011fac57a3cb5
Mario Dubreuil <mario_dubreuil@hotmail.com>
2022-09-30 16:31:01 -0400

    Add booking service

---
+++ FlyingDutchmanAirlines/ServiceLayer/BookingService.cs
@@ -0,0 +1,20 @@
+﻿using FlyingDutchmanAirlines.RepositoryLayer;
+
+namespace FlyingDutchmanAirlines.ServiceLayer;
+
+public class BookingService
+{
+    private readonly BookingRepository _bookingRepository;
+    private readonly CustomerRepository _customerRepository;
+    private readonly FlightRepository _flightRepository;
+    public BookingService(BookingRepository bookingRepository, CustomerRepository customerRepository, FlightRepository flightRepository)
+    {
+        _bookingRepository = bookingRepository;
+        _customerRepository = customerRepository;
+        _flightRepository = flightRepository;
+    }
+    public async Task<(bool, Exception)> CreateBooking(int customerId, int flightNumber)
+    {
+        return (true, null!);
+    }
+}
---
+++ FlyingDutchmanAirlines_Tests/ServiceLayer/BookingServiceTests.cs
@@ -0,0 +1,15 @@
+﻿namespace FlyingDutchmanAirlines_Tests.ServiceLayer;
+
+[TestClass]
+public class BookingServiceTests
+{
+    [TestInitialize]
+    public async Task TestInitialize()
+    {
+    }
+
+    [TestMethod]
+    public async Task CreateBooking_Success()
+    {
+    }
+}
