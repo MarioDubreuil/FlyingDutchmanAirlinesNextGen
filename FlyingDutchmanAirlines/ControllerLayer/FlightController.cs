4a138beaa0aedb5e1f0acf092638423f68a2a8d3
Mario Dubreuil <mario_dubreuil@hotmail.com>
2022-10-05 20:57:10 -0400

    Add flight controller

---
+++ FlyingDutchmanAirlines/ControllerLayer/FlightController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace FlyingDutchmanAirlines.ControllerLayer
+{
+    public class FlightController : ControllerBase
+    {
+        public IActionResult GetFlights()
+        {
+            return StatusCode((int)HttpStatusCode.OK, "Hello, World!");
+        }
+    }
+}
---
+++ FlyingDutchmanAirlines_Tests/ControllerLayer/FlightControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using FlyingDutchmanAirlines.ControllerLayer;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlyingDutchmanAirlines_Tests.ControllerLayer
+{
+    [TestClass]
+    public class FlightControllerTests
+    {
+        [TestMethod]
+        public void GetFlights_Success()
+        {
+            var controller = new FlightController();
+            var response = controller.GetFlights() as ObjectResult;
+            Assert.IsNotNull(response);
+            Assert.AreEqual((int)HttpStatusCode.OK, response.StatusCode);
+            Assert.AreEqual("Hello, World!", response.Value);
+        }
+    }
+}
