5ee23c36efbb4152cd7b151794d79585823be13d
Mario Dubreuil <mario_dubreuil@hotmail.com>
2022-10-06 17:46:33 -0400

    Add dependency injection

--- FlyingDutchmanAirlines/ControllerLayer/FlightController.cs
+++ FlyingDutchmanAirlines/ControllerLayer/FlightController.cs
@@ -1,10 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FlyingDutchmanAirlines.ServiceLayer;
+using Microsoft.AspNetCore.Mvc;
 using System.Net;
 
 namespace FlyingDutchmanAirlines.ControllerLayer
 {
     public class FlightController : ControllerBase
     {
+        private readonly FlightService _service;
+
+        public FlightController(FlightService service)
+        {
+            _service = service;
+        }
+
         public IActionResult GetFlights()
         {
             return StatusCode((int)HttpStatusCode.OK, "Hello, World!");
--- FlyingDutchmanAirlines/Program.cs
+++ FlyingDutchmanAirlines/Program.cs
@@ -1,8 +1,17 @@
+using FlyingDutchmanAirlines.DatabaseLayer;
+using FlyingDutchmanAirlines.RepositoryLayer;
+using FlyingDutchmanAirlines.ServiceLayer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddTransient(typeof(FlightService), typeof(FlightService));
+builder.Services.AddTransient(typeof(FlightRepository), typeof(FlightRepository));
+builder.Services.AddTransient(typeof(AirportRepository), typeof(AirportRepository));
+builder.Services.AddTransient(typeof(FlyingDutchmanAirlinesContext), typeof(FlyingDutchmanAirlinesContext));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
