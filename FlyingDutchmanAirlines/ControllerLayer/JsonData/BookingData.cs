eddf3c151eb41d4e08301151ad06313b675b768a
Mario Dubreuil <mario_dubreuil@hotmail.com>
2022-10-08 09:46:41 -0400

    Add BookingData

---
+++ FlyingDutchmanAirlines/ControllerLayer/JsonData/BookingData.cs
@@ -0,0 +1,22 @@
+﻿namespace FlyingDutchmanAirlines.ControllerLayer.JsonData;
+
+public class BookingData
+{
+    private string _firstName = null!;
+    private string _lastName = null!;
+
+    public string FirstName
+    {
+        get => _firstName;
+        set => _firstName = ValidateName(value, nameof(FirstName));
+    }
+
+    public string LastName
+    {
+        get => _lastName;
+        set => _lastName = ValidateName(value, nameof(LastName));
+    }
+
+    private string ValidateName(string name, string propertyName) =>
+        string.IsNullOrEmpty(name) ? throw new InvalidOperationException($"Could not set {propertyName}") : name;
+}
---
+++ FlyingDutchmanAirlines_Tests/ControllerLayer/JsonData/BookingDataTests.cs
@@ -0,0 +1,50 @@
+﻿using FlyingDutchmanAirlines.ControllerLayer.JsonData;
+
+namespace FlyingDutchmanAirlines_Tests.ControllerLayer.JsonData;
+
+[TestClass]
+public class BookingDataTests
+{
+    [TestMethod]
+    public void BookingData_Success()
+    {
+        string firstName = "Marina";
+        string lastName = "Michaels";
+        var bookingData = new BookingData
+        {
+            FirstName = firstName,
+            LastName = lastName
+        };
+        Assert.IsNotNull(bookingData);
+        Assert.AreEqual(firstName, bookingData.FirstName);
+        Assert.AreEqual(lastName, bookingData.LastName);
+    }
+
+    [TestMethod]
+    [DataRow("", "")]
+    [DataRow("Marina", "")]
+    [DataRow("", "Michales")]
+    [ExpectedException(typeof(InvalidOperationException))]
+    public void BookingData_Failure_EmptyName(string firstName, string lastName)
+    {
+        var bookingData = new BookingData
+        {
+            FirstName = firstName,
+            LastName = lastName
+        };
+    }
+
+    [TestMethod]
+    [DataRow(null, null)]
+    [DataRow("Marina", null)]
+    [DataRow(null, "Michales")]
+    [ExpectedException(typeof(InvalidOperationException))]
+    public void BookingData_Failure_NullName(string firstName, string lastName)
+    {
+        var bookingData = new BookingData
+        {
+            FirstName = firstName,
+            LastName = lastName
+        };
+    }
+}
