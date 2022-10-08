5b2840d30f1f9955be1acfdaae8f6535c70fba01
Mario Dubreuil <mario_dubreuil@hotmail.com>
2022-10-08 09:57:35 -0400

    Add BookingController

---
+++ FlyingDutchmanAirlines/ControllerLayer/BookingController.cs
@@ -0,0 +1,23 @@
+﻿using FlyingDutchmanAirlines.ControllerLayer.JsonData;
+using FlyingDutchmanAirlines.ServiceLayer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlyingDutchmanAirlines.ControllerLayer;
+
+[ApiController]
+[Route("[controller]")]
+public class BookingController : ControllerBase
+{
+    private readonly BookingService _service;
+
+	public BookingController(BookingService service)
+	{
+		_service = service;
+	}
+
+	//[HttpPost]
+	//public async Task<IActionResult> CreateBooking([FromBody] BookingData body)
+	//{
+
+	//}
+}
--- FlyingDutchmanAirlines/ControllerLayer/FlightController.cs
+++ FlyingDutchmanAirlines/ControllerLayer/FlightController.cs
@@ -4,61 +4,60 @@ using FlyingDutchmanAirlines.Views;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
 
-namespace FlyingDutchmanAirlines.ControllerLayer
+namespace FlyingDutchmanAirlines.ControllerLayer;
+
+[ApiController]
+[Route("[controller]")]
+public class FlightController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class FlightController : ControllerBase
+    private readonly FlightService _service;
+
+    public FlightController(FlightService service)
     {
-        private readonly FlightService _service;
+        _service = service;
+    }
 
-        public FlightController(FlightService service)
+    [HttpGet]
+    public async Task<IActionResult> GetFlights()
+    {
+        var flights = new Queue<FlightView>();
+        try
         {
-            _service = service;
+            await foreach (var flight in _service.GetFlights())
+            {
+                flights.Enqueue(flight);
+            }
+            return StatusCode((int)HttpStatusCode.OK, flights);
         }
-
-        [HttpGet]
-        public async Task<IActionResult> GetFlights()
+        catch (FlightNotFoundException)
         {
-            var flights = new Queue<FlightView>();
-            try
-            {
-                await foreach (var flight in _service.GetFlights())
-                {
-                    flights.Enqueue(flight);
-                }
-                return StatusCode((int)HttpStatusCode.OK, flights);
-            }
-            catch (FlightNotFoundException)
-            {
-                return StatusCode((int)HttpStatusCode.NotFound, "No flights were found in the database");
-            }
-            catch (Exception)
-            {
-                return StatusCode((int)HttpStatusCode.InternalServerError, "An error occurred");
-            }
+            return StatusCode((int)HttpStatusCode.NotFound, "No flights were found in the database");
         }
-
-        [HttpGet("{flightNumber}")]
-        public async Task<IActionResult> GetFlightByFlightNumber(int flightNumber)
+        catch (Exception)
         {
-            try
-            {
-                var flight = await _service.GetFlightByFlightNumber(flightNumber);
-                return StatusCode((int)HttpStatusCode.OK, flight);
-            }
-            catch (FlightNotFoundException)
-            {
-                return StatusCode((int)HttpStatusCode.NotFound, "Flight was not found in the database");
-            }
-            catch (ArgumentException)
-            {
-                return StatusCode((int)HttpStatusCode.BadRequest, "Bad request");
-            }
-            catch (Exception)
-            {
-                return StatusCode((int)HttpStatusCode.InternalServerError, "An error occurred");
-            }
+            return StatusCode((int)HttpStatusCode.InternalServerError, "An error occurred");
+        }
+    }
+
+    [HttpGet("{flightNumber}")]
+    public async Task<IActionResult> GetFlightByFlightNumber(int flightNumber)
+    {
+        try
+        {
+            var flight = await _service.GetFlightByFlightNumber(flightNumber);
+            return StatusCode((int)HttpStatusCode.OK, flight);
+        }
+        catch (FlightNotFoundException)
+        {
+            return StatusCode((int)HttpStatusCode.NotFound, "Flight was not found in the database");
+        }
+        catch (ArgumentException)
+        {
+            return StatusCode((int)HttpStatusCode.BadRequest, "Bad request");
+        }
+        catch (Exception)
+        {
+            return StatusCode((int)HttpStatusCode.InternalServerError, "An error occurred");
         }
     }
 }
